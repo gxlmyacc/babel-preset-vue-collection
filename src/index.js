b155346de2f6bed4bd8f8420dc9cb98fd403cab3
gxlmyacc <gxl.mymail@163.com>
2018-09-15 12:59:52 +0800

    back to babel 6

--- src/index.js
+++ src/index.js
@@ -8,24 +8,26 @@ module.exports = function (context, options) {
   delete options.polyfill;
 
   let presets = [
-    [require('@babel/preset-env').default, options],
+    [require('babel-preset-env').default, options],
     // vue jsx
     require.resolve('babel-preset-vue')
   ];
 
   let plugins = [
+    polyfill ? [require.resolve('babel-plugin-polyfill-env'), options] : null,
     // Polyfills the runtime needed for async/await and generators
-    [require.resolve('@babel/plugin-transform-runtime'), {
+    [require.resolve('babel-plugin-transform-runtime'), {
       helpers: !useBuiltIns && !polyfill,
+      polyfill: !useBuiltIns && !polyfill,
       regenerator: true,
       // Resolve the Babel runtime relative to the config.
-      // moduleName: path.dirname(require.resolve('@babel/runtime/package'))
+      moduleName: path.dirname(require.resolve('babel-runtime/package'))
     }],
     [require('babel-plugin-transform-object-rest-spread'), {
       useBuiltIns: useBuiltIns
     }],
     // For dynamic import that you will use a lot in code-split
-    require.resolve('@babel/plugin-syntax-dynamic-import'),
+    require.resolve('babel-plugin-syntax-dynamic-import'),
     // class properties transforme
     require.resolve('babel-plugin-transform-class-properties'),
   ].filter(Boolean);
